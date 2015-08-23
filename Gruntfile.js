7823e7efc9d4b7604f3958ec77b12b0d108ac069
Ryan Kelly <94ryan.kelly@gmail.com>
2015-08-23 15:50:21 +0100

    Wired up unit test runner mocha rather than nodeunit and removed dependencies. Made the project work with visual studio code out of the box with building and testing, with a default download folder for running also without any dimension changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,17 @@ module.exports = function(grunt) {
     require('time-grunt')(grunt);
     // Load all grunt tasks
     require('load-grunt-tasks')(grunt);
-
+ 
     // Project configuration.
     grunt.initConfig({
-        nodeunit: {
-            files: ['test/**/*_test.js']
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt'
+                },
+                src: ['test/**/*.js']
+            }
         },
         jshint: {
             options: {
@@ -33,16 +39,20 @@ module.exports = function(grunt) {
             },
             lib: {
                 files: '<%= jshint.lib.src %>',
-                tasks: ['jshint:lib', 'nodeunit']
+                tasks: ['jshint:lib', 'mochaTest:test']
             },
             test: {
                 files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'nodeunit']
+                tasks: ['jshint:test', 'mochaTest:test']
             }
         }
     });
 
     // Default task.
-    grunt.registerTask('default', ['jshint', 'nodeunit']);
+    grunt.registerTask('default', ['jshint', 'mochaTest:test']);
+    
+    grunt.registerTask('build', ['jshint']);
+    
+    grunt.registerTask('test', ['mochaTest:test']);
 
 };
--- test/download-test.js
+++ test/download-test.js
@@ -2,16 +2,15 @@
 
 var assert = require('assert');
 var _ = require('lodash');
-var getChromecastBackgrounds = require('./index');
+var getChromecastBackgrounds = require('../index');
 
 it('should get some background objects', function(cb) {
     getChromecastBackgrounds().then(function (backgrounds) {
-        console.log(backgrounds);
         assert(backgrounds);
         var background = backgrounds[0];
         assert(_.has(background, 'url', 'author'));
         assert(background.url.indexOf('googleusercontent') > 0);
         cb();
     });
-
 });
+
